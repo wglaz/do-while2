7063aa55e0deeceab50b21d6b869c8ff34fd9c3f
DanielKubanek <danielkubanek@gmail.com>
2018-10-26 12:02:48 +0200

    takie uzycie do while

---
+++ while.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int main() {
+    int liczba;
+    int licznik_petli = 0;
+    char x;
+
+    printf("Program liczy największą liczbę mniejszą od\n");
+    printf("liczby podanej przez użytkownika, która\n");
+    printf("jest niepodzielna przez 2, 3, 5, 7.\n\n");
+    do {
+        printf("Podaj liczbę: ");
+        scanf(" %d", &liczba);
+
+        // zrób nieparzystą
+        if (!(liczba % 2)) --liczba;
+
+        for (; !(liczba % 3) || !(liczba % 5) || !(liczba % 7); liczba -= 2) {
+            licznik_petli++;
+        }
+        printf("znaleziona liczba: %d\n", liczba);
+
+        printf("\n");
+        printf("liczba pętli wyniosła: %d\n", licznik_petli);
+
+        printf("Czy chcesz wykonać operację jeszcze raz: TAK - t NIE - n\n");
+        scanf(" %c", &x);
+    }while(x=='t'|| x=='T');
+    return 0;
+}
